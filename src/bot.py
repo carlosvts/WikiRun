7533da0854b531681440fa1edac006bdbaf9e42e
carlosvtsdev@gmail.com <carlosvtsdev@gmail.com>
2023-10-04 21:07:50 -0300

    running the bot for the first time

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,17 @@
+import os
+
+from dotenv import load_dotenv
 from interactions import Client, Intents, SlashContext, listen, slash_command
+
+load_dotenv()
 
 bot = Client(intents=Intents.DEFAULT)
 
+
+@listen()
+async def on_ready():
+    print("Bot is ready")
+
+
+# running the bot
+bot.start(os.getenv('BOT-TOKEN'))
