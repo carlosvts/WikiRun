bd2a46560b4a776fab8db6b56958e28ea08f40c1
carlosvtsdev@gmail.com <carlosvtsdev@gmail.com>
2023-10-08 22:54:29 -0300

    func get_related_titles implemented

--- src/wikipediafetcher.py
+++ src/wikipediafetcher.py
@@ -1,4 +1,5 @@
 import os
+from typing import Type
 
 import requests
 from dotenv import load_dotenv
@@ -9,7 +10,7 @@ load_dotenv()
 client_id = os.getenv('CLIENT-ID')
 access_token = {"access_token": os.getenv('ACCESS-TOKEN')}
 
-endpoint = "http://en.wikipedia.org/w/api.php"
+basepoint = "http://en.wikipedia.org/w/api.php"
 
 client = Client(client_id=client_id,
                 access_token=access_token)
@@ -19,13 +20,39 @@ auth = OAuth2(
     token=client.access_token
 )
 
-data = {}
+data: dict[type, type] = {}
 
 headers = {
     "Authorization": f"Bearer {access_token}"
 }
 
-r = requests.get(url='https://en.wikipedia.org/w/api.php',
-                 data=data, auth=auth, headers=headers)
+# r = requests.get(url='https://en.wikipedia.org/w/api.php',
+#                 data=data, auth=auth, headers=headers)
 
-print(r.status_code)
+
+def get_related_titles(article_title):
+    _params = {
+        "action": "query",
+        "format": "json",
+        "prop": "links",
+        "titles": article_title,
+        "pllimit": 50
+    }
+
+    response = requests.get(
+        basepoint, params=_params
+    )
+
+    def _fetch_info(data):
+        _all_links = data['query']['pages']['6690']['links']
+        print(_all_links)
+        queue = []
+        for link in _all_links:
+            link_title = link['title']
+            queue.append(link_title)
+        return queue
+
+    _fetch_info(response.json())
+
+
+print(get_related_titles("Coca-Cola"))
